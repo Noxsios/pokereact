47167270bfb41a90cd11beddb52fb0e2d8de3daa
noxsios <harryrndzz@gmail.com>
2020-06-18 21:51:32 +0200

    Improved search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import Search from './pokeComponents/search'
-// import PokeList from './pokeComponents/pokelist'
-import {Container}  from '@material-ui/core'
+import { Container } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import SearchAppBar from './pokeComponents/SearchAppBar';
 
 function App() {
   return (
-    <div className="App">
-      <Container maxWidth="sm">
-        <Search />
+    <div>
+      {/* <SearchAppBar /> */}
+    <Container maxWidth="sm">
+      <Paper variant="outlined">
+          <div style={{ display: 'flex', justifyContent: 'center' }}><Search /></div>
         {/* <PokeList /> */}
-      </Container>
+      </Paper>
+      <Button variant="outlined" size="large" color="primary">
+        &laquo; Previous 
+        </Button>
+        <Button id="button-next" variant="outlined" size="large" color="primary">
+        Next &raquo;
+        </Button>
+    </Container>
     </div>
   );
 }
--- src/pokeComponents/search.js
+++ src/pokeComponents/search.js
@@ -1,34 +1,115 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { TextField } from '@material-ui/core'
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 // var Pokedex = require("pokedex-promise-v2");
 import Pokedex from 'pokedex-promise-v2'
+import PokeCard from './pokeCard'
 
-export default class Search extends Component {
+// export default class Search extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            textFieldValue: '',
-            pokemonTypes: '',
-            pokemonID: null,
-            pokemonPic: '',
-            pokemonMoves: []
-        }
-    }
+//     constructor() {
+//         super()
+//         this.state = {
+//             textFieldValue: '',
+//             name: "",
+//             pokemonTypes: '',
+//             pokemonID: null,
+//             pokemonPic: '',
+//             pokemonMoves: [],
+//             renderSearch: false
+//         }
+//     }
 
-    handleChange = (event) => {
-        this.setState({ ...this.state, textFieldValue: event.target.value })
+//     handleChange = (event) => {
+//         this.setState({ ...this.state, textFieldValue: event.target.value })
+//         // this.setState({ ...this.state, pokemonProperties: event.target.value })
+//         console.log(this.state.textFieldValue)
+//     }
+
+//     pokeSearch = (event) => {
+//         if (event.key === "Enter") {
+
+//             event.preventDefault()
+
+//             let capLine = this.state.textFieldValue[0].toUpperCase() + this.state.textFieldValue.slice(1);
+
+//             this.setState({ ...this.state, name: capLine, renderSearch : true })
+
+//             var options = {
+//                 protocol: "https",
+//                 versionPath: "/api/v2/",
+//                 cacheLimit: 5 * 1000, // 2s
+//                 timeout: 5 * 1000, // 5s
+//             };
+
+//             var P = new Pokedex(options);
+
+//             P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+//                 .then((response) =>
+//                     response.types
+//                         .map((type) => type.type)
+//                         .map((inner) => inner.name)
+//                         .join(", ")
+//                 )
+//                 .then((output) => this.setState({ ...this.state, pokemonTypes: output }))
+//                 .catch((error) => console.log("There was an ERROR: ", error));
+
+//             P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+//                 .then((response) => response.id)
+//                 .then((output) => this.setState({ ...this.state, pokemonID: output }))
+//                 .catch((error) => console.log("There was an ERROR: ", error));
+
+//             P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+//                 .then((response) => response.sprites.front_default)
+//                 .then((output) => this.setState({ ...this.state, pokemonPic: output }))
+//                 .catch((error) => console.log("There was an ERROR: ", error));
+
+//             P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+//                 .then((response) => response.moves.map(ele => ele.move.name))
+//                 .then((output) => this.setState({ ...this.state, pokemonMoves: output }))
+//                 .catch((error) => console.log("There was an ERROR: ", error));
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Pokemon Search</h3>
+//                 <form /*className={classes.root}*/ noValidate autoComplete="off">
+//                     <TextField id="filled-basic" color="secondary" label="Search" variant="filled" onKeyDown={this.pokeSearch} onChange={this.handleChange} />
+//                 </form>
+//                 {this.state.renderSearch && <PokeCard name={this.state.name} id={this.state.pokemonID} type={this.state.pokemonTypes} moves={this.state.pokemonMoves} pic={this.state.pokemonPic} />}
+//             </div>
+//         )
+//     }
+// }
+
+const Search = () => {
+
+    const [textFieldValue, setText] = useState('')
+    const [name, setname] = useState('')
+    const [pokemonTypes, setpokemonTypes] = useState('')
+    const [pokemonID, setpokemonID] = useState(null)
+    const [pokemonPic, setpokemonPic] = useState('')
+    const [pokemonMoves, setpokemonMoves] = useState([])
+    const [renderSearch, setrenderSearch] = useState(false)
+    const [stats, setstats] = useState([])
+
+    let handleChange = (event) => {
+        setText(event.target.value)
         // this.setState({ ...this.state, pokemonProperties: event.target.value })
-        console.log(this.state.textFieldValue)
+        // console.log(this.state.textFieldValue)
+        console.log(textFieldValue)
     }
 
-    pokeSearch = (event) => {
+    let pokeSearch = (event) => {
         if (event.key === "Enter") {
 
             event.preventDefault()
+
+            let capLine = textFieldValue[0].toUpperCase() + textFieldValue.slice(1);
+
+            setname(capLine)
+            setrenderSearch(true)
 
             var options = {
                 protocol: "https",
@@ -39,62 +120,49 @@ export default class Search extends Component {
 
             var P = new Pokedex(options);
 
-            P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+            P.getPokemonByName(textFieldValue.toLocaleLowerCase()) // with Promise
                 .then((response) =>
                     response.types
                         .map((type) => type.type)
                         .map((inner) => inner.name)
                         .join(", ")
                 )
-                .then((output) => this.setState({ ...this.state, pokemonTypes: output }))
+                .then((output) => setpokemonTypes(output))
                 .catch((error) => console.log("There was an ERROR: ", error));
 
-            P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+            P.getPokemonByName(textFieldValue.toLocaleLowerCase()) // with Promise
                 .then((response) => response.id)
-                .then((output) => this.setState({ ...this.state, pokemonID: output }))
+                .then((output) => setpokemonID(output))
                 .catch((error) => console.log("There was an ERROR: ", error));
 
-            P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+            P.getPokemonByName(textFieldValue.toLocaleLowerCase()) // with Promise
                 .then((response) => response.sprites.front_default)
-                .then((output) => this.setState({ ...this.state, pokemonPic: output }))
+                .then((output) => setpokemonPic(output))
                 .catch((error) => console.log("There was an ERROR: ", error));
 
-            P.getPokemonByName(this.state.textFieldValue.toLocaleLowerCase()) // with Promise
+            P.getPokemonByName(textFieldValue.toLocaleLowerCase()) // with Promise
                 .then((response) => response.moves.map(ele => ele.move.name))
-                .then((output) => this.setState({ ...this.state, pokemonMoves: output }))
+                .then((output) => setpokemonMoves(output))
                 .catch((error) => console.log("There was an ERROR: ", error));
+
+            P.getPokemonByName(textFieldValue.toLocaleLowerCase()) // with Promise
+                .then((response) => response.stats.map((type) => type.base_stat))
+                .then((output) => setstats(output))
+                .catch((error) => console.log("There was an ERROR: ", error));
+
         }
     }
 
-    render() {
-        return (
-            <div>
-                <h3>Pokemon Search</h3>
-                <form /*className={classes.root}*/ noValidate autoComplete="off">
-                    <TextField id="filled-basic" label="Search" variant="filled" onKeyDown={this.pokeSearch} onChange={this.handleChange} />
-                </form>
-                <h2>{this.state.textFieldValue}</h2>
-                <img src={this.state.pokemonPic} alt="Pokemon Front Default"></img>
-                <h3>ID: {this.state.pokemonID}</h3>
-                <h3>Type(s): {this.state.pokemonTypes}</h3>
-                <FormControl>
-                    <InputLabel htmlFor="filled-age-native-simple">Moves</InputLabel>
-                    <Select
-                        native
-                        labelId="demo-simple-select-label"
-                        id="moves-select"
-                    // value={age}
-                    // onChange={handleChange}
-                    >
-                        <option aria-label="None" value="" />
-                        <option>Moves Below:</option>
-                        {this.state.pokemonMoves.sort().map(item => (<option value={item} >{item}</option>))}
-                    </Select>
-                </FormControl>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h3>Pokemon Search</h3>
+            <form /*className={classes.root}*/ noValidate autoComplete="off">
+                <TextField id="filled-basic" color="secondary" label="Search" variant="filled" onKeyDown={pokeSearch} onChange={handleChange} />
+            </form>
+            {renderSearch && <PokeCard name={name} id={pokemonID} type={pokemonTypes} moves={pokemonMoves} pic={pokemonPic} stats={stats} />}
+        </div>
+    )
 }
 
-
+export default Search
 
